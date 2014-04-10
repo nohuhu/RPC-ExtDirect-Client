678c0fce132df8ec95394a6a6bce73807cfa8847
Alex Tokarev <nohuhu@nohuhu.org>
2014-04-09 23:10:19 -0700

    Beefed up test server a bit

--- t/server.pl
+++ t/server.pl
@@ -1,16 +1,30 @@
 #!/usr/bin/perl
 
 # This script can be used for manual HTTP server testing in case
-# something goes awry
+# something goes awry. Note that it does NOT fork so should be
+# run in a separate terminal session.
+#
+# This script is naive in its assumption that a randomly generated port
+# will be available for listening. If it's not, restart the script
+# manually.
 
-use common::sense;
+use strict;
+use warnings;
+
+use Getopt::Std;
 
 use RPC::ExtDirect::Server;
 
-my $server = RPC::ExtDirect::Server->new(static_dir => 't/htdocs');
-my $port   = $server->port;
+my %opt = ( h => 'localhost', p => 30000 + int rand 10000 );
+getopt('hp', \%opt) if @ARGV;
 
-say "Listening on port $port";
+my $server = RPC::ExtDirect::Server->new(
+    host       => $opt{h},
+    port       => $opt{p},
+    static_dir => 't/htdocs',
+);
+
+print "Listening on $opt{h}:$opt{p}\n";
 
 $server->run();
 
